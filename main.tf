6878d467c3c84d337c900a5ce978bf601429bee8
Milos <58978044+miloszizic@users.noreply.github.com>
2022-10-28 16:17:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -405,7 +405,7 @@ module "lambda_ec2_backup" {
 
   function_name  = "backup_ec2_lambda"
   create_package = false
-  description = "Lambda function for EC2 instances backups"
+  description = "Lambda function for EC2 instances backups and"
   handler                  = "scripts/${var.go_backup_filename}"
   store_on_s3              = true
   role_name                = "lambda_ami_backup_role"
